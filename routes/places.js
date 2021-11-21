04a2c831bdc9a211dc093deec9f552d0f5a1d1d2
Aarón López <n_aaron5@hotmail.com>
2021-11-21 14:40:13 +0100

    Search info for One place

--- routes/places.js
+++ routes/places.js
@@ -7,23 +7,26 @@ const auth_key = Buffer.from(`06158c46a81fe6ca54de872a59d59bec:2c9853ff551e804fa
 //Models
 
 
-
-
-// const axiosCall = await axios(
-//     `https://api.roadgoat.com/api/v2/destinations/auto_complete?q=paris`,
-//     {
-//         headers: {
-//             'Authorization': `Basic ${auth_key}`
-//         }
-//     }
-// )
-// console.log(axiosCall.data.data[0])
-
 //Render places view
 router.get('/places', async (req, res, next) => {
    res.render('places/places')
 })
 
+router.get('/places/:id', async (req, res, next) => {
+    try{
+        const axiosCall = await axios(
+            `https://api.roadgoat.com/api/v2/destinations/${req.params.id}`,
+            {
+                headers: {
+                    'Authorization': `Basic ${auth_key}`
+                }
+            }) 
+            console.log(axiosCall.data.included)
+    } catch(err){
+        console.log(chalk.bgRed(err))
+    }
+   
+})
 //POST route for search places
 router.post('/places', async (req, res, next) => {
     try{
@@ -35,7 +38,6 @@ router.post('/places', async (req, res, next) => {
         }
     })
     const places = axiosCall.data.data
-    console.log(places)
     res.render('places/places', { places })
     } catch(err){
         console.log(chalk.bgRed(err))
